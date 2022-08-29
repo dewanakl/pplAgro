88d40bb6b41c2985f9767d6f9cd27a4216bedf8f
dewanakl <53048107+dewanakl@users.noreply.github.com>
2022-08-29 09:53:38 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,8 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if ($this->app->environment('production')) {
-            URL::forceScheme('https');
-        }
+        $this->app['request']->server->set('HTTPS', 'on'); // this line
+        URL::forceScheme('https');
     }
 }
